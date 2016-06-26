d3279f893934f0dc640e47f54ef93a50baec7787
Shan Ting <vxst@vxst.org>
2016-06-26 23:18:23 +0800

    Some more pages

--- scripts/login.js
+++ scripts/login.js
@@ -11,4 +11,32 @@ $("#user_login_btn").click(()=>{
 			'password': $("#password").val()
 		})
 	})
-})
+}).then((response)=>{
+	var result = response.json();
+	if(result['status'] == 'ok'){
+		$(location).attr('href', '/trade/index.html');
+	}else{
+		alert("登录失败");
+	}
+});
+$("#admin_login_btn").click(()=>{
+	fetch('/api/user/login',{
+		method: 'POST',
+		credentials: 'same-origin',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			'username': $("#username").val(),
+			'password': $("#password").val()
+		})
+	})
+}).then((response)=>{
+	var result = response.json();
+	if(result['status'] == 'ok'){
+		$(location).attr('href', '/admin/index.html');
+	}else{
+		alert("登录失败");
+	}
+});
