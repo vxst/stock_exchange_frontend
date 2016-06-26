e6f94d027092b744a566312c90a1a0b3500b1f3e
Shan Ting <vxst@vxst.org>
2016-06-26 22:20:57 +0800

    Fix cookie

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,7 @@
 $("#user_login_btn").click(()=>{
 	fetch('/api/user/login',{
 		method: 'POST',
+		credentials: 'same-origin',
 		headers: {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json'
